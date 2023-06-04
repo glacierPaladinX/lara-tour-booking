88a973c9cfcbd0a7ee02cd1fb03f3ccaadf8043a
glacierPaladinX <achillesgit@outlook.com>
2023-06-04 13:03:32 +0100

    Product Model Completed

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'type', 'description', 'capacity'];
+
+    /**
+     * Get the bookings of this product
+     */
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    /**
+     * Check if the product is available for booking
+     *
+     * @return bool
+     */
+    public function isAvailable()
+    {
+        return $this->bookings_count < $this->capacity;
+    }
 }
