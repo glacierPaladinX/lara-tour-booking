f28a98889a6035d7248621f46decab641b55f15a
glacierPaladinX <achillesgit@outlook.com>
2023-06-04 12:45:59 +0100

    Client Model completed

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['first_name', 'last_name', 'email', 'passport_num', 'gender'];
+
+    /**
+     * Get the bookings made by this client.
+     */
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    /**
+     * Check if the client has already booked the given product.
+     *
+     * @param  int  $productId
+     * @return bool
+     */
+    public function hasBookedProduct($productId)
+    {
+        return $this->bookings()
+            ->where('product_id', $productId)
+            ->exists();
+    }
+
 }
