6f4127517963a6891649854e61e7a8c5c6b8f370
glacierPaladinX <achillesgit@outlook.com>
2023-06-04 13:11:40 +0100

    Booking model completed

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['client_id', 'product_id', 'booked_on'];
+
+    /**
+     * Get client who booked this
+     */
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    /**
+     * Get product which is booked
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
