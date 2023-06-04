8ec5aade7cb46a3329a939e3bb7e448a1b2f5e00
glacierPaladinX <achillesgit@outlook.com>
2023-06-04 13:20:55 +0100

    ClientController.php completed

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class ClientController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        $clients = Client::with('bookings')->get();
+
+        return response()->json([
+            'data' => $clients,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email|unique:clients',
+            'passport_num' => 'required',
+            'gender' => 'required',
+        ]);
+
+        $client = Client::create($request->all());
+
+        return response()->json([
+            'message' => 'Client created successfully.',
+            'data' => $client,
+        ], 201);
+    }
 }
